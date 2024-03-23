eb2accbd17b078b2b435b5718a52a36d9bd6bd9f
AndriyMerzlikin <lastheroofme@gmail.com>
2024-03-23 11:05:06 +0200

    styling card - descr

--- src/components/Catalog/CampersCard/CampersCard.jsx
+++ src/components/Catalog/CampersCard/CampersCard.jsx
@@ -1,15 +1,25 @@
+import sprite from "../../../assets/sprite.svg";
 import {
   CarImg,
   CardContainer,
   CardInfoBox,
   CardPrice,
   CardTitle,
+  DescriptionText,
+  LocationBox,
+  LocationText,
+  ReviewsBox,
+  ReviewsInfoBox,
+  ReviewsLink,
   TitleInfoBox,
   TitleInfoLine,
 } from "./CampersCard.styled";
 
 // eslint-disable-next-line react/prop-types
-export const CampersCard = ({ value: { name, gallery, price } }) => {
+export const CampersCard = ({
+  // eslint-disable-next-line react/prop-types
+  value: { name, gallery, price, location, reviews, rating, description },
+}) => {
   return (
     <CardContainer>
       <CarImg src={gallery[0]} alt="car" />
@@ -19,7 +29,27 @@ export const CampersCard = ({ value: { name, gallery, price } }) => {
             <CardTitle>{name}</CardTitle>
             <CardPrice>{price}</CardPrice>
           </TitleInfoLine>
+          <ReviewsInfoBox>
+            <ReviewsBox>
+              <svg width="16px" height="16px">
+                <use xlinkHref={sprite + "#star"} />
+              </svg>
+              <ReviewsLink href="#">
+                <p>
+                  <span>{rating}</span>({reviews.length} Reviews)
+                </p>
+              </ReviewsLink>
+            </ReviewsBox>
+            <LocationBox>
+              <svg width="16px" height="16px">
+                <use xlinkHref={sprite + "#map-pin"} />
+              </svg>
+              <LocationText>{location}</LocationText>
+            </LocationBox>
+          </ReviewsInfoBox>
         </TitleInfoBox>
+
+        <DescriptionText>{description}</DescriptionText>
       </CardInfoBox>
     </CardContainer>
   );
--- src/components/Catalog/CampersCard/CampersCard.styled.jsx
+++ src/components/Catalog/CampersCard/CampersCard.styled.jsx
@@ -27,12 +27,14 @@ export const CardInfoBox = styled.div`
 export const TitleInfoBox = styled.div`
   width: 526px;
   height: 62px;
+  margin-bottom: 24px;
 `;
 
 export const TitleInfoLine = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  margin-bottom: 8px;
 `;
 
 export const CardTitle = styled.h2`
@@ -47,4 +49,53 @@ export const CardPrice = styled.p`
   font-size: 24px;
   line-height: 125%;
   color: ${(p) => p.theme.colors.black};
+`;
+
+// reviews box
+
+export const ReviewsInfoBox = styled.div`
+  /* width: 267px; */
+  height: 24px;
+  display: flex;
+  gap: 16px;
+`;
+
+export const ReviewsBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+`;
+
+export const ReviewsLink = styled.a`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+  text-decoration: underline;
+  text-decoration-skip-ink: none;
+  color: ${(p) => p.theme.colors.black};
+`;
+
+export const LocationBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+`;
+
+export const LocationText = styled.div`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+  color: ${(p) => p.theme.colors.black};
+`;
+
+// text descriotion
+
+export const DescriptionText = styled.p`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 150%;
+  color: ${(p) => p.theme.colors.grey};
+  overflow: hidden;
+  white-space: nowrap; /* Запобігає переносу тексту */
+  text-overflow: ellipsis;
 `;
