4c7fb677c895a251d5d630cc816a152e20e7dbeb
AndriyMerzlikin <lastheroofme@gmail.com>
2024-03-24 22:55:21 +0200

    removed console

---
+++ assets/FavoritesPage-BPDcB5IO.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-_9fZY8L9.js";const s=()=>e.jsx("div",{children:e.jsx("h1",{children:"FavoritesPage"})});export{s as default};
---
+++ assets/HomePage-Cw0vFuf3.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-_9fZY8L9.js";const o=()=>e.jsx("div",{children:e.jsx("h1",{children:"Home Page"})});export{o as default};
