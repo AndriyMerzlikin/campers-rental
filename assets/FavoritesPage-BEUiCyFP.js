5ba6eecd207e05b61db578661c4cbd59e830e833
AndriyMerzlikin <lastheroofme@gmail.com>
2024-03-27 21:35:17 +0200

    red line added

---
+++ assets/FavoritesPage-BEUiCyFP.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D3oF7Ku2.js";const s=()=>e.jsx("div",{children:e.jsx("h1",{children:"FavoritesPage"})});export{s as default};
---
+++ assets/HomePage-DvQSYWmA.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D3oF7Ku2.js";const o=()=>e.jsx("div",{children:e.jsx("h1",{children:"Home Page"})});export{o as default};
